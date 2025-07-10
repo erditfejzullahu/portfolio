fdb4bc17faa9897a6b96e82f3abffbd355f97b31
Erdit Fejzullahu <erditfejzullahu45@gmail.com>
2025-07-10 04:06:11 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,35 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     optimizeCss: true,
+    optimizeServerReact: true
   },
   compiler: {
     styledComponents: true,
     reactRemoveProperties: true,
     removeConsole: process.env.NODE_ENV ===  "production"
+  },
+  compress: true,
+  async headers() {
+    return [
+      {
+        source: '/_next/static/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      },
+      {
+        source: "/static/(.*)", // Caches images/fonts
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, must-revalidate",
+          },
+        ],
+      },
+    ]
   }
   /* config options here */
 };
